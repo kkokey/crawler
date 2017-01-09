a71c664eb1deb5cd8d93caa03fa5d24950628741
kkokey <kkokey@gmail.com>
2017-01-10 02:49:41 +0900

    [MOD] DB connection function modify

--- myCon.py
+++ myCon.py
@@ -1,37 +1,50 @@
 import mysql.connector
 import ConfigParser
 
-ppConfig = ConfigParser.RawConfigParser()
-ppConfig.read(r'../config.properties')
+class DbConnection():
 
-#config = {
-#	'user': configUser,
-#	'password': configPass,
-#	'host': configHost,
-#	'database': configDb,
-#	'raise_on_warnings': configRow,
-#	'use_pure': configIsPure,
-#}
+	def getConfig():
+		config = {}
+		
+		ppConfig = ConfigParser.RawConfigParser()
+		ppConfig.read(r'../config.properties')
 
-config = {}
-config['user'] = ppConfig.get("SectionDB", "user")
-config['password'] = ppConfig.get("SectionDB", "password")
-config['host'] = ppConfig.get("SectionDB", "host")
-config['database'] = ppConfig.get("SectionDB", "database")
-config['raise_on_warnings'] = eval(ppConfig.get("SectionDB","raise_on_warnings"))
-config['use_pure'] = ppConfig.get("SectionDB", "use_pure")
+		config['user'] = ppConfig.get("SectionDB", "user")
+		config['password'] = ppConfig.get("SectionDB", "password")
+		config['host'] = ppConfig.get("SectionDB", "host")
+		config['database'] = ppConfig.get("SectionDB", "database")
+		config['raise_on_warnings'] = eval(ppConfig.get("SectionDB","raise_on_warnings"))
 
+## Perhaps add option??
+#config['pool_size'] = 5
+#config['use_pure'] = ppConfig.get("SectionDB", "use_pure")
 
-cnx = mysql.connector.connect(**config)
-cursor = cnx.cursor()
+		return config
 
-query = ("SELECT host, user, password  FROM user")
+	def getDbPool(dbConfig):
+		cnxPool = mysql.connector.connect(pool_name="myPool", **dbConfig)
 
-rs = cursor.execute(query)
+## another option?
+#cnxPool = mysql.connector.connect(pool_name="myPool", pool_size="5")
+#cnxPooling = mysql.connector.pooling.MySQLConnectionPool(pool_name = "myPool", pool_size = "3", **dbConfig)
 
-for (host, user, password) in cursor:
-		print("'{}', '{}', '{}'".format(host, user, password))
+		return cnxPool
 
-cnx.close()
+	def selectQuery(cnx, strQuery):
+		if not strQuery:
+			strQuery = ("SELECT host, user, password  FROM user")
+
+		cursor = cnx.cursor()
+		rs = cursor.execute(strQuery)
+
+		for (host, user, password) in cursor:
+				print("'{}', '{}', '{}'".format(host, user, password))
+
+	def dbClose(cnx):
+		cnx.close()
+	
+	print("##### GO")
+	selectQuery(getDbPool(getConfig()), "")
+
 
 
