409bd3914f9e55794df421e839163a59ff6c25a8
kkokey <kkokey@gmail.com>
2017-01-11 23:27:34 +0900

    [MOD] Change file env

--- crawl/spiders/run_crawl.sh
+++ crawl/spiders/run_crawl.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
 PW="(rootpw)"
+SCRAPY_PATH="/usr/local/virtualenv/ENV/bin/scapy"
 
+if [ -d $SCRAPY_PATH ]; then
+	echo -e $PW+"\n" | sudo -S /usr/local/virtualenv/ENV/bin/scrapy crawl crawl_page
+else
+	echo -e $PW+"\n" | sudo -S scrapy crawl crawl_page
+fi
 
-echo -e $PW+"\n" | sudo -S /usr/local/virtualenv/ENV/bin/scrapy crawl crawl_page
+echo -e $PW+"\n" | sudo -S scrapy crawl crawl_page
+
